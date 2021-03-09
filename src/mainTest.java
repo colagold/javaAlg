d60c2d98d2a06c42a2e59a68594d5520fc6a2faf
Yangfighting <1346380661@qq.com>
2021-03-10 00:29:22 +0800

    更新两个排序算法

--- src/mainTest.java
+++ src/mainTest.java
@@ -1,18 +1,20 @@
+import LinkList.sortAlg;
+
 import java.awt.*;
 
 public class mainTest {
-    private static int cou;
-    public static void main(String[] args) {
-        try {
-            m();
-        }catch (Throwable e){
-            e.printStackTrace();
-            System.out.println(cou);
+    public static void outArray(int[] array){
+        for (int i:array
+             ) {
+            System.out.print(i+" ");
         }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        int[] a={2,8,500,9,7,-1,88};
+        outArray(new sortAlg().bubbleSort(a));
+        outArray(new sortAlg().selectSort(a));
     }
-    public static void m(){
-        cou++;
-        m();
-    }
+
 }

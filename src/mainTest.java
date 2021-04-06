42d77b90f85d9c8d6b90ebec9b800db4fa594c95
Yangfighting <1346380661@qq.com>
2021-04-06 08:10:23 +0800

    更改readme

--- src/mainTest.java
+++ src/mainTest.java
@@ -1,6 +1,7 @@
 import LinkList.sortAlg;
 
 import java.awt.*;
+import java.io.*;
 
 public class mainTest {
     public static void outArray(int[] array){
@@ -11,10 +12,22 @@ public class mainTest {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        int[] a={2,8,500,9,7,-1,88};
-        outArray(new sortAlg().bubbleSort(a));
-        outArray(new sortAlg().selectSort(a));
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("F:\\Resources\\Code\\keras训练图像识别数据集\\label.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        BufferedWriter out = new BufferedWriter(new FileWriter("label.txt"));
+
+
+        String str = null;
+        while((str = bufferedReader.readLine()) != null)
+        {
+            out.write(str.split(",")[1].split(" ")[0]+"/可回收物"+"\n");
+        }
+        //close
+        inputStream.close();
+        bufferedReader.close();
+        out.close();
+
     }
 
 }

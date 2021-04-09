a489bbe2d4a5ffd57597fff7b610c277d10ad34b
Yangfighting <1346380661@qq.com>
2021-04-09 22:51:59 +0800

    链表基本操作

--- src/DataStructure/LinkList/reverseList.java
+++ src/DataStructure/LinkList/reverseList.java
@@ -2,7 +2,9 @@ package DataStructure.LinkList;
 
 public class reverseList {
     public ListNode reverseList(ListNode head) {
-        if(head.next==null) return head;
+        if(head.next==null) {
+            return head;
+        }
         ListNode last=reverseList(head.next);
         return null;
     }
